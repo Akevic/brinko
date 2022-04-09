e323d378cb9626bd0f40734bf8c6f99b3a1b1cdf
andrija <andrija@andrias-MacBook-Pro.local>
2022-04-09 16:27:57 +0200

    update design

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Tilt from 'react-tilt'
-import brain from './brain.png'
+import fish from './fish.png'
 
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
       <Tilt className='Tilt br2' options={{ max: 55 }} style={{ height: 100, width: 100 }} >
         <div className='Tilt-inner pa3'>
-          <img src={brain} alt='logo' />
+          <img src={fish} alt='logo' />
         </div>
       </Tilt>
     </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,10 +5,17 @@ import FaceRecognition from '../components/FaceRecognition/FaceRecognition'
 import Particles from 'react-particles-js'
 import Clarifai from 'clarifai'
 import './App.css'
+// import { ClarifaiStub, grpc } from 'clarifai-nodejs-grpc'
+// const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc")
 
 const app = new Clarifai.App({
-  apiKey: '78440aba0cc6408c9dcaa2eca78e9d9d'
+  apiKey: '0416a2bad12d45e39da79d38ea4be083'
 })
+
+// const stub = app.grpc
+
+// const metadata = stub.Metadata()
+// metadata.set("authorization", "Key YOUR_CLARIFAI_API_KEY")
 
 const particlesOptions = {
   particles: {
@@ -31,11 +38,11 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    fetch('https://melodious-marshmallow-2ae870.netlify.app:3000')
-      .then(response => response.json())
-      .then(console.log)
-  }
+  // componentDidMount() {
+  //   fetch('https://melodious-marshmallow-2ae870.netlify.app:3000')
+  //     .then(response => response.json())
+  //     .then(console.log)
+  // }
 
   onInputChange = (e) => {
     const imageLink = e.target.value
@@ -45,8 +52,9 @@ class App extends Component {
   onButtonSubmit = () => {
     let patternName
     let patternProbability
-    app.models.predict(Clarifai.TEXTURES_AND_PATTERNS, this.state.image).then(
+    app.models.predict(Clarifai.GENERAL_MODEL, this.state.image).then(
       function (response) {
+        console.log(response)
         patternName = response.outputs[0].data.concepts[0].name 
         patternProbability = Math.round(+response.outputs[0].data.concepts[0].value * 100)
       },
