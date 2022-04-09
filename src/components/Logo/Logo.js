11ac7aac693154ff14f6ae01d56460fdf0979d46
andrija <andrija@andrias-MacBook-Pro.local>
2022-04-09 17:42:48 +0200

    update logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Tilt from 'react-tilt'
-import fish from './fish.png'
+import logo from './logo.png'
 
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
       <Tilt className='Tilt br2' options={{ max: 55 }} style={{ height: 100, width: 100 }} >
         <div className='Tilt-inner pa3'>
-          <img src={fish} alt='logo' />
+          <img src={logo} alt='logo' />
         </div>
       </Tilt>
     </div>
